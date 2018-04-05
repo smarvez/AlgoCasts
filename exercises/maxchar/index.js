abddcbef30bd91bedb4ab2dd2611b42a22d5dcc8
Stephanie Marvez <marvez@gmail.com>
2018-04-04 18:58:16 -0600

    maxchar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let arr = str.split("");
+  let obj = {};
+  let max = 0;
+  let maxChar = '';
+  arr.forEach(el => {
+    (obj[el]) ? obj[el]++ : obj[el] = 1;
+  })
+  for (let key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
